4e0eedcf35b6dcdd90e5a70e78e9e76dad11709d
Nika Vikentyeva <nika.vikentyeva@gmail.com>
2022-11-22 16:34:07 +0300

    added test task

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# MODESTOFLOWERS-test-task
+# MODESTO FLOWERS test task
+
+### Dev
+
+In the project directory, you can run:
+
+```bash
+npm run dev
+```
+
+Runs the app in the development mode.
+Open http://localhost:8000/ to view it in the browser.
+
+### Prod
+
+```bash
+npm run build
+```
+
+Builds the app for production to the build folder.
---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+import '../scss/app.scss';
+
+// Toggle menu
+
+const menuBurger = document.querySelector(".header__burger");
+const user = document.querySelectorAll(".header__icon");
+const nav = document.querySelector(".menu");
+const navUser = document.querySelector(".header__user");
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+if (menuBurger) {
+    menuBurger.addEventListener("click", function (e) {
+        menuBurger.classList.toggle("active");
+        nav.classList.toggle("active");
+        sidebar.classList.toggle("active");
+        body.classList.toggle("lock");
+        if (navUser.classList.contains("active")) {
+            body.classList.remove("lock");
+            menuBurger.classList.remove("active");
+            navUser.classList.remove("active")
+            nav.classList.remove("active");
+            sidebar.classList.remove("active");
+        }
+    });
+}
+
+if (user) {
+    user.forEach(item  => {
+        item.addEventListener("click", function (e) {
+            menuBurger.classList.toggle("active");
+            navUser.classList.add("active")
+            body.classList.add("lock");
+            if (!menuBurger.classList.contains("active")) {
+                menuBurger.classList.add("active")
+            }
+        })
+    })
+
+}
+
